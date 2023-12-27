a6b2242319a651334860541a6c20e3849499035d
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-12-27 08:45:37 -0300

    update audio service

--- src/main/java/com/crowdar/tella/services/AudioService.java
+++ src/main/java/com/crowdar/tella/services/AudioService.java
@@ -25,7 +25,7 @@ public class AudioService {
         MobileActionManager.waitVisibility(AudioConstants.MICROPHONE_ICON);
         MobileActionManager.click(AudioConstants.MICROPHONE_ICON);
         acceptPermissions();
-        MobileActionManager.click(AudioConstants.MICROPHONE_ICON);
+        //MobileActionManager.click(AudioConstants.MICROPHONE_ICON);
     }
 
     public static void clickPencilIcon(){
