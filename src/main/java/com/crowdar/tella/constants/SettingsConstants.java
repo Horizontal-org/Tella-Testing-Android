60220d4d9aea5913036a8de2ffcf025c56cd0e45
Lisa Acosta <31046847+Lisa-Acosta@users.noreply.github.com>
2024-07-13 11:37:02 -0300

    Settings.feature - Scenario View Settings Categories -

--- src/main/java/com/crowdar/tella/constants/SettingsConstants.java
+++ src/main/java/com/crowdar/tella/constants/SettingsConstants.java
@@ -9,4 +9,9 @@ public class SettingsConstants {
     public static final String BUTTON_BACK_LANG = "id:btn_back";
     public static final String TITLE_LANGUAGE_SETTING = "id:language_setting";
 
+    public static final String CATEGORY_SETTINGS_TITLE = "id:startTitleTv";
+    public static final String SECURITY_BUTTON = "id:security_settings_button";
+    public static final String SERVERS_BUTTON = "id:servers_settings_button";
+    public static final String ABOUT_HELP_BUTTON = "id:about_n_help_settings_button";
+    public static final String FEEDBACK_BUTTON = "id:feedback_button";
 }
--- src/main/java/com/crowdar/tella/services/SettingsService.java
+++ src/main/java/com/crowdar/tella/services/SettingsService.java
@@ -7,25 +7,28 @@ import io.appium.java_client.MobileBy;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SettingsService {
 
-    public static void clickSettingsIcon(){
+    public static void clickSettingsIcon() {
         MobileActionManager.waitVisibility(SettingsConstants.SETTINGS_ICON);
         MobileActionManager.click(SettingsConstants.SETTINGS_ICON);
     }
 
-    public static void clickGeneralIcon(){
+    public static void clickGeneralIcon() {
         MobileActionManager.waitVisibility(SettingsConstants.GENERAL_BUTTON);
         MobileActionManager.click(SettingsConstants.GENERAL_BUTTON);
     }
-    public static void clickLanguageOptions(){
+
+    public static void clickLanguageOptions() {
         MobileActionManager.waitVisibility(SettingsConstants.LANGUAGE_BUTTON);
         MobileActionManager.click(SettingsConstants.LANGUAGE_BUTTON);
     }
 
-    public static void verifyListOfLanguages(){
+    public static void verifyListOfLanguages() {
         MobileActionManager.waitVisibility(SettingsConstants.LANGUAGE_LIST);
         Assert.assertTrue(MobileActionManager.isVisible(SettingsConstants.LANGUAGE_LIST));
     }
@@ -38,7 +41,7 @@ public class SettingsService {
         pedidoEle.click();
     }
 
-    public static void verifyLanguageTitle(String title){
+    public static void verifyLanguageTitle(String title) {
         MobileActionManager.waitVisibility(SettingsConstants.LANGUAGE_TITLE);
         Assert.assertTrue(MobileActionManager.isVisible(SettingsConstants.LANGUAGE_TITLE));
         String LanguageTitle = MobileActionManager.getText(SettingsConstants.LANGUAGE_TITLE);
@@ -47,5 +50,27 @@ public class SettingsService {
         MobileActionManager.click(SettingsConstants.BUTTON_BACK_LANG);
         MobileActionManager.waitVisibility(SettingsConstants.TITLE_LANGUAGE_SETTING);
         Assert.assertTrue(MobileActionManager.isVisible(SettingsConstants.TITLE_LANGUAGE_SETTING));
+    }
+
+    public static void clickCategory(String category) {
+        Map<String, String> links = new HashMap<>();
+        links.put("General", SettingsConstants.GENERAL_BUTTON);
+        links.put("Security", SettingsConstants.SECURITY_BUTTON);
+        links.put("Servers", SettingsConstants.SERVERS_BUTTON);
+        links.put("About & Help", SettingsConstants.ABOUT_HELP_BUTTON);
+        links.put("Feedback", SettingsConstants.FEEDBACK_BUTTON);
+        try {
+            MobileActionManager.waitVisibility(links.get(category));
+            MobileActionManager.click(links.get(category));
+        } catch (Exception e) {
+            MobileActionManager.click(links.get(category));
+        }
+    }
+
+    public static void accessCategory(String category) {
+        MobileActionManager.waitVisibility(SettingsConstants.CATEGORY_SETTINGS_TITLE);
+        Assert.assertTrue(MobileActionManager.isVisible(SettingsConstants.CATEGORY_SETTINGS_TITLE));
+        String LanguageTitle = MobileActionManager.getText(SettingsConstants.CATEGORY_SETTINGS_TITLE);
+        Assert.assertEquals(LanguageTitle, category);
     }
 }
