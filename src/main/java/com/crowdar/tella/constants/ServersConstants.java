6f4cb0936bede29f5609f4ba12deca8a91c62856
Lisa Acosta <31046847+Lisa-Acosta@users.noreply.github.com>
2024-07-24 13:01:10 -0300

    test: Servers.feature - Updated -

---
+++ src/main/java/com/crowdar/tella/constants/ServersConstants.java
@@ -0,0 +1,11 @@
+package com.crowdar.tella.constants;
+
+public class ServersConstants {
+
+    public static final String VIEW_NOT_DISPLAYED_MESSAGE = "Home page is not displayed.";
+    public static final String APP_MSG = "id:txv_msg";
+    public static final String HOME_BUTTON = "id:home";
+    public static final String CAMERA_BUTTON = "id:camera";
+    public static final String MIC_BUTTON = "id:mic";
+
+}
---
+++ src/main/java/com/crowdar/tella/steps/ServersSteps.java
@@ -0,0 +1,13 @@
+package com.crowdar.tella.steps;
+
+import com.crowdar.core.PageSteps;
+import com.crowdar.tella.services.ServersService;
+import io.cucumber.java.en.When;
+
+
+public class ServersSteps extends PageSteps {
+    @When("the user presses the + button")
+    public void theUserPressesTheButton() {
+        ServersService.clickPlusButton();
+    }
+}
