0a63c8f022aead6660d6bddfa3e14422b8816e9c
Lisa Acosta <31046847+Lisa-Acosta@users.noreply.github.com>
2024-07-24 18:51:14 -0300

    test: Servers.feature - Select Server -

--- src/main/java/com/crowdar/tella/steps/ServersSteps.java
+++ src/main/java/com/crowdar/tella/steps/ServersSteps.java
@@ -1,19 +1,40 @@
 package com.crowdar.tella.steps;
-
 import com.crowdar.core.PageSteps;
 import com.crowdar.tella.services.ServersService;
+import com.crowdar.tella.services.SettingsService;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.List;
 
 public class ServersSteps extends PageSteps {
     @When("the user presses the + button")
     public void theUserPressesTheButton() {
         ServersService.clickPlusButton();
     }
-
     @Then("the user sees all possible server options available for connection")
-    public void theUserSeesAllPossibleServerOptionsAvailableForConnection() {
-        ServersService.viewConectionsServerOptions();
+    public void theUserSeesAllPossibleServerOptionsAvailableForConnection(List<String> listServer) {
+        ServersService.viewConectionsServerOptions(listServer);
+    }
+
+    @And("^clicks on the option (.*)$")
+    public void clicksOnTheOptionsServers(String option) {
+        SettingsService.clickCategory(option);
+    }
+
+    @And("^selects the server (.*)$")
+    public void selectsTheServer(String server) {
+        ServersService.selectServer(server);
+    }
+
+    @And("^the user presses \"(.*)\"$")
+    public void theUserPresses(String button) {
+        ServersService.pressButton(button);
+    }
+
+    @Then("^the user enters the tella web server (.*) settings$")
+    public void theUserEntersTheTellaWebServerSettings(String server) {
+        ServersService.viewSettingServer(server);
     }
 }
