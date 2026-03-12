72592137002e7f3cbdb6dabea5beca78abfaf0fb
Abel Godoy <abelgodoy.1802@gmail.com>
2026-03-12 19:24:13 -0300

    Se corrigen pruebas rotas y casuistica E2E en feature Lock

--- src/main/java/com/crowdar/tella/constants/LockUnlockConstants.java
+++ src/main/java/com/crowdar/tella/constants/LockUnlockConstants.java
@@ -18,7 +18,7 @@ public class LockUnlockConstants {
     public static final String LOCK_PATTERN_ID = "id:pl_pattern";
     public static final String SCREEN_PATTERN_ID = "id:pl_message_text";
     public static final String NETX_BUTTON_PATTERN_ID = "id:pl_right_button";
-    public static final String LOCK_SET_UP = "id:sheet_subtitle";
+    public static final String LOCK_SET_UP = "id:org.hzontal.tella:id/onboard_title";
     public static final String EXIT_BUTTON = "id:right_img";
     public static final String TELLA_APP = "ACCESSIBILITY_ID:Tella";
     public static final String VIEW_NOT_DISPLAYED_MESSAGE = "Login page is not displayed.";
