66ed67b90fe1d8642e2d69a2acf04302e1e95abf
BrianAzcona <brianazcona45@gmail.com>
2026-03-17 11:34:00 -0300

    Final de casos fallidos corregidos

--- src/main/java/com/crowdar/tella/services/UnlockService.java
+++ src/main/java/com/crowdar/tella/services/UnlockService.java
@@ -53,6 +53,7 @@ public class UnlockService {
         MobileActionManager.click(LockUnlockConstants.PASSWORD_RIGHT_BUTTON);
         MobileActionManager.click(LockUnlockConstants.CONTINUE_BUTTON);
         MobileActionManager.click(LockUnlockConstants.PASSWORD_NEXT_BUTTON);
+        MobileActionManager.waitVisibility(LockUnlockConstants.START_BUTTON);
         Assert.assertTrue(MobileActionManager.isVisible(LockUnlockConstants.START_BUTTON), LockUnlockConstants.VIEW_NOT_DISPLAYED_MESSAGE);
     }
 
